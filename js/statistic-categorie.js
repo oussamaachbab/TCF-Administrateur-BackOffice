50ca8aee4c855a70c555d9edbe33d010d4e288a2
ettaoussi <ettaoussiilyas0.1@gmail.com>
2024-11-15 16:31:59 +0100

    just some change

--- js/statistic-categorie.js
+++ js/statistic-categorie.js
@@ -34,6 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     Object.keys(localStorage).forEach(key => {
         try {
+            if (key === 'admin') return;
             const user = JSON.parse(localStorage.getItem(key));
             if (user && user.username) {
                 const option = document.createElement('option');
